1526ede37c4d309eb5c6427013cc482ba3489953
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-12 22:17:17 -0300

    One more

---
+++ SomaSImples.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SomaSImples {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número inteiro: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Digite o segundo número inteiro: ");
+        int b = scanner.nexInt();
+
+        int soma = a + b;
+        System.out.println("A soma é: " + soma);
+
+        scanner.close();
+    }
+}
