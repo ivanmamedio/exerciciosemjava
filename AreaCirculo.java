d2679f11afbd7ad43782dce74cf6390aebdebf01
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-12 22:58:14 -0300

    Terminando mais um

--- AreaCirculo.java
+++ AreaCirculo.java
@@ -1,5 +1,15 @@
 import java.util.Scanner;
 
 public class AreaCirculo {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("DIgite o raio do círculo: ");
+        double raio = scanner.nextDouble();
+
+        double area = Math.PI * raio * raio;
+        System.out.printf("Área do círculo: %.2f%n", area);
+
+        scanner.close();
+    }
 }
