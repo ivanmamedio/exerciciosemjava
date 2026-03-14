cd0f8cea5ffd88f2ceb6cdaaa25738eaf414a84b
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-13 21:11:15 -0300

    Another

---
+++ Estruturas condicionais (if-else, switch)/MaiorDeDois.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MaiorDeDois {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        double a = scanner.nextDouble();
+
+        
+    }
+}
