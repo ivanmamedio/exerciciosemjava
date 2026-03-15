10dd1c26576dbbee186bc51ea4a3b21e120723a4
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-15 13:36:42 -0300

    Update

--- Estruturas condicionais (if-else, switch)/MaiorDeDois.java
+++ Estruturas condicionais (if-else, switch)/MaiorDeDois.java
@@ -7,6 +7,15 @@ public class MaiorDeDois {
         System.out.print("Digite o primeiro número: ");
         double a = scanner.nextDouble();
 
-        
+        System.out.print("Digite o segundo número: ");
+        double b = scanner.nextDouble();
+
+        if (a > b)
+            System.out.println("O maior número é: " + a);
+    }   else if (b > a) {
+            System.out.println("O maior número é: " + b);
+    }    else {
+            System.out.println("Os números são iguais.");
+    }
     }
 }
