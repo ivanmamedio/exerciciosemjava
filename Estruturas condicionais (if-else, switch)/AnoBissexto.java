742eb04605a9cec1e600165621e5841c13d42d89
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-16 22:42:56 -0300

    Test

--- Estruturas condicionais (if-else, switch)/AnoBissexto.java
+++ Estruturas condicionais (if-else, switch)/AnoBissexto.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class AnoBissexto {
-    
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um ano: ");
+        int ano = scanner.nextInt();
+
+        boolean bissexto = (ano % 400 == 0) || (ano % 4 == 0 && ano & 100 != 0);
+
+        if (bissexto) {
+            System.out.println(ano + " é um ano bissexto.");
+        } else {
+            System.out.println(ano + " não é um ano bissexto.");
+        }
+        scanner.close();
+    }
 }
