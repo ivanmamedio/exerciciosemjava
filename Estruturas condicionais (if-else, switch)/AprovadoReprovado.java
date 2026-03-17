acaf297b36033b426a9509acc0c9d885173b1294
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-16 22:24:50 -0300

    Finish

--- Estruturas condicionais (if-else, switch)/AprovadoReprovado.java
+++ Estruturas condicionais (if-else, switch)/AprovadoReprovado.java
@@ -5,5 +5,21 @@ public class AprovadoReprovado {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Digite a primeira nota: ");
+        double nota1 = scanner.nextDouble();
+
+        System.out.print("Digite a segunda nota: ");
+        double nota2 = scanner.nextDouble();
+
+        double media = (nota1 + nota2) / 2;
+        System.out.printf("Média: %.2f%n", media);
+
+        if (media >= 7.0) {
+            System.out.println("Aprovado!");
+        } else if (media >= 5.0) {
+            System.out.println("Recuperação!");
+        } else {
+            System.out.println("Reprovado!");
+        }
+        scanner.close();
     }
 }
