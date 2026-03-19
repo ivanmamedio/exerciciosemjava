dfd7425a065ffec9f6c9507acc5400354ec58e24
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-18 22:24:30 -0300

    I hadn’t actually done anything with do/while until now.

--- Estruturas condicionais (if-else, switch)/MenuOpcoes.java
+++ Estruturas condicionais (if-else, switch)/MenuOpcoes.java
@@ -1,5 +1,12 @@
 import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class MenuOpcoes {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+
+
+    }
 }
