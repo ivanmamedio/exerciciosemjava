58b88d4f033185095bff7e9c2685ba7612bdf8b9
Carlos Ivan Mamédio <carlos.ivan1997@outlook.com>
2026-03-19 10:04:09 -0300

    First steps into the world of loops
    I’m not on my computer right now, so I’ll just throw the files up as I get the chance.

---
+++ Laços de repetição (for, while, do-while)/ContagemRegressiva.java
@@ -0,0 +1,10 @@
+public class ContagemRegressiva {
+    public static void main(String[] args) {
+        int contador = 10;
+        while (contador >= 1) {
+            System.out.println(contador);
+            contador--;
+            }
+            System.out.println("Fim!");
+        }
+    }
