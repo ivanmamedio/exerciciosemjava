e34327ad1362b955a48f78a7620c1beb49c7ca4a
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-18 22:30:38 -0300

    A bit more complicated than i thought it'd be

--- Estruturas condicionais (if-else, switch)/MenuOpcoes.java
+++ Estruturas condicionais (if-else, switch)/MenuOpcoes.java
@@ -5,8 +5,19 @@ import java.time.format.DateTimeFormatter;
 public class MenuOpcoes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        int opcao;
 
+        do {
+            System.out.println("\n--- Menu ---");
+            System.out.println("1 - Saudação");
+            System.out.println("2 - Mostrar data");
+            System.out.println("3 - Sair");
+            System.out.print("Escolha uma opção: ");
 
+            opcao = scanner.nextInt();
+            scanner.nextLine(); // limpar o buffer
+            
+
+            
     }
 }
