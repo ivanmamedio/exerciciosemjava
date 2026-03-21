0c201cc881309e2d4391fdfe17856864c0a2b28c
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-20 21:33:22 -0300

    .

--- Laços de repetição (for, while, do-while)/Fatorial.java
+++ Laços de repetição (for, while, do-while)/Fatorial.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 
 public class Fatorial {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número inteiro positivo: ");
+        int n = scanner.nextInt();
 
+        if (n < 0) {
+            System.out.println("Não existe fatorial para número negativo.");
+        } else {
+            long fatorial = 1;
+            for (int i = 1; i <= n; i++) {
+                fatorial *= i;
+            }
+            System.out.println(n + "! = " + fatorial);
+        }
+
+        scanner.close();
     }
 }
