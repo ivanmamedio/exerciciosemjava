7c95accf964900d0a84989ba39e85f553a8a8853
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-20 22:00:29 -0300

    .

--- Laços de repetição (for, while, do-while)/Fibonacci.java
+++ Laços de repetição (for, while, do-while)/Fibonacci.java
@@ -1,1 +1,11 @@
-import
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Quantos termos da sequência de Fibonacci deseja?");
+        int n = scanner.nextInt();
+
+        if
+    }
+}
