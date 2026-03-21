9f370e48909990236a4193e07e75856628371963
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-20 22:07:02 -0300

    Atualizando

---
+++ Laços de repetição (for, while, do-while)/ValidacaoSenha.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class ValidacaoSenha {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String senha;
+    }
+}
