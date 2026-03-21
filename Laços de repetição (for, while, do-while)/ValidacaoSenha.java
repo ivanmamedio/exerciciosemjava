0c70d7f980b04566baeffdbeb0ffcb6e73c9e053
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-20 22:10:58 -0300

    Pronto

--- Laços de repetição (for, while, do-while)/ValidacaoSenha.java
+++ Laços de repetição (for, while, do-while)/ValidacaoSenha.java
@@ -4,5 +4,16 @@ public class ValidacaoSenha {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String senha;
+
+        do {
+            System.out.print("Digite a senha: ");
+            senha = scanner.nextLine();
+            if (!senha.equals("1234")) {
+                System.out.println("Senha incorreta. Tente novamente.");
+            }
+        } while (!senha.equals("1234"));
+
+        System.out.println("Acesso permitido!");
+        scanner.close();
     }
 }
