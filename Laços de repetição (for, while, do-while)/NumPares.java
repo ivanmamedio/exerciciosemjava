64136ac8f831a29e5c6737125e435e283ad9aa86
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-20 21:23:38 -0300

    .

--- Laços de repetição (for, while, do-while)/NumPares.java
+++ Laços de repetição (for, while, do-while)/NumPares.java
@@ -1,6 +1,9 @@
 public class NumPares {
     public static void main(String[] args) {
         System.out.println("Números pares de 0 a 100:");
-        for 
+        for (int i = 0; i <= 100; i += 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println(); // apenas para pular linha no final
     }
 }
