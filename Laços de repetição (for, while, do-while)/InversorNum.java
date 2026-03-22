bf6394ec1a0bb81d676cbaa777bbef7ce68a9927
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-22 01:36:43 -0300

    Atualizando

--- Laços de repetição (for, while, do-while)/InversorNum.java
+++ Laços de repetição (for, while, do-while)/InversorNum.java
@@ -1,1 +1,19 @@
 import java.util.Scanner;
+
+public class InversorNum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.prit("Digite um número inteiro: ");
+        int numero = scanner.nextInt();
+
+        int invertido = 0;
+        int original = numero;
+        numero = Math.abs(numero); // para lidar com números negativos
+
+        while (numero > 0) {
+            int digito = numero % 10;
+            invertido = invertido * 10 + digito;
+            numero /= 10;
+        }
+    }
+}
