74b74f5003883976b76171cf90c3a51deaa05a9d
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-22 15:49:13 -0300

    Erros corrigidos

--- Vetores (arrays) e Strings/ProcurandoElemento.java
+++ Vetores (arrays) e Strings/ProcurandoElemento.java
@@ -2,12 +2,32 @@ import java.util.Scanner;
 
 public class ProcurandoElemento {
     public static void main(String[] args) {
-        Scanner scanner = new scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int[] numeros = new int[10];
 
         System.out.println("Digite 10 números: ");
         for (int i = 0; i < numeros.length; i++) {
             numeros[i] = scanner.nextInt();
         }
+
+        System.out.print("Digite o número a ser procurado: ");
+        int alvo = scanner.nextInt();
+
+        boolean encontrado = false;
+        System.out.print("Posições encontradas: ");
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] == alvo) {
+                System.out.print(i + " ");
+                encontrado = true;
+            }
+        }
+
+        if (!encontrado) {
+            System.out.println("Número não está presente no array.");
+        } else {
+            System.out.println();
+        }
+
+        scanner.close();
     }
 }
