503f7238b0bd265dbe0e2e680613391e41e40c8d
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-22 10:27:55 -0300

    Finalizando. Achei bem diferente esse

--- Laços de repetição (for, while, do-while)/NumPerf.java
+++ Laços de repetição (for, while, do-while)/NumPerf.java
@@ -1,6 +1,17 @@
 public class NumPerf {
     public static void main(String[] args) {
         System.out.println("Número perfeitos entre 1 e 1000:");
-        for (int num = 1; num <= 1000; num++)
+        for (int num = 1; num <= 1000; num++) {
+            int somaDivisores = 0;
+            for (int i = 1; i <= num / 2; i++) {
+                if (num % i == 0) {
+                    somaDivisores += i;
+                }
+            }
+            if (somaDivisores == num) {
+                System.out.print(num + " ");
+            }
+        }
+        System.out.println(); // linha final
     }
 }
