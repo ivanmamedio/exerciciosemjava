80295a1830f6f5cbea81a6ecd9726fc7aa6a2a84
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-22 19:13:50 -0300

    Finalizado

--- Vetores e Arrays/ContandoVogais.java
+++ Vetores e Arrays/ContandoVogais.java
@@ -1,1 +1,19 @@
 import java.util.Scanner;
+
+public class ContandoVogais {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite uma frase: ");
+        String frase = scanner.nextLine().toLowerCase();
+
+        int contador = 0;
+        for (int i = 0; i < frase.length(); i++) {
+            char c = frase.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                contador++;
+            }
+        }
+        System.out.println("Número de vogais: " + contador);
+        scanner.close();
+    }
+}
