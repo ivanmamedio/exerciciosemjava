2da50d2fe9ab951c891808f3e81d9082b44245ba
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-22 01:24:27 -0300

    Atualizando

---
+++ Laços de repetição (for, while, do-while)/DomaDigitos.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class DomaDigitos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número inteiro: ");
+        int numero = scanner.nextInt();
+    }
+}
