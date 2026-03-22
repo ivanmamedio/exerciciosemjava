ec70d0cf403c9f18de2f1f5cddfebad63a0a4752
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-22 10:33:20 -0300

    Corrigindo erro

--- Laços de repetição (for, while, do-while)/Multiplos3e5.java
+++ Laços de repetição (for, while, do-while)/Multiplos3e5.java
@@ -2,10 +2,10 @@ public class Multiplos3e5 {
     public static void main(String[] args) {
         int soma = 0;
         for (int i = 1; i <= 500; i++) {
-            if (i % 3 == 0 || i % 5 == 0 ) {
-                soma ++ i;
+            if (i % 3 == 0 || i % 5 == 0) {
+                soma += i;
             }
         }
-    System.out.println("Soma de múltiplos de 3 ou 5 entre 1 e 500: " + soma);
+        System.out.println("Soma de múltiplos de 3 ou 5 entre 1 e 500: " + soma);
     }
 }
