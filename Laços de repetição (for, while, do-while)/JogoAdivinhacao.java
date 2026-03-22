c68cf4cc46baf64a7b5732fb62b38b0aa19d522e
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-22 02:06:35 -0300

    Mais um

---
+++ Laços de repetição (for, while, do-while)/JogoAdivinhacao.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class JogoAdivinhacao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+    }
+}
