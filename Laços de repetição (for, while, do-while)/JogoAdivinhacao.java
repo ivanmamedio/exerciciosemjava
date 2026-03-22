8e73908a583a8d85e2f070056a1610d9b4cd195e
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-22 02:11:43 -0300

    Finanlizando e começando correções

--- Laços de repetição (for, while, do-while)/JogoAdivinhacao.java
+++ Laços de repetição (for, while, do-while)/JogoAdivinhacao.java
@@ -4,5 +4,27 @@ public class JogoAdivinhacao {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+
+        int numeroSecreto = random.nextInt(100) + 1; // entre 1 e 100
+        int palpite;
+        int tentativas = 0;
+
+        System.out.println("Tente adivinhar o número entre 1 e 100!");
+
+        do {
+            System.out.print("Seu palpite: ");
+            palpite = scanner.nextInt();
+            tentativas++;
+
+            if (palpite < numeroSecreto) {
+                System.out.println("Maior! Tente novamente.");
+            } else if (palpite > numeroSecreto) {
+                System.out.println("Menor! Tente novamente!");
+            } else {
+                System.out.printçn("Parabéns! Você acertou em " + tentativas + "tentativas");
+            }
+        } while (palpite != numeroSecreto);
+
+        scanner.close();
     }
 }
