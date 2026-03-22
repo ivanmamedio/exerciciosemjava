4752d21d3aa54fcb35ba82d603cf2c1fb7d989af
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-22 01:22:15 -0300

    Finalizando e começando a testar

--- Laços de repetição (for, while, do-while)/NumPrimos.java
+++ Laços de repetição (for, while, do-while)/NumPrimos.java
@@ -3,5 +3,26 @@ import java.util.Scanner;
 public class NumPrimos {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número inteiro positivo: ");
+        int num = scanner.nextInt();
+
+        if (num <= 1) {
+            System.out.println(num + " não é primo.");
+        } else {
+            boolean primo = true;
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    primo = false;
+                    break;
+                }
+            }
+
+            if (primo) {
+                System.out.println(num + " é primo.");
+            } else {
+                System.out.println(num + " não é primo.");
+            }
+        }
+        scanner.close();
     }
 }
