a989a25c5ca57502e9b6f037cbe690da0731da58
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-22 10:19:34 -0300

    Atualizando

--- Laços de repetição (for, while, do-while)/DesenhoRetangulo.java
+++ Laços de repetição (for, while, do-while)/DesenhoRetangulo.java
@@ -1,3 +1,9 @@
-public class DesenhoRetangulo {
+import java.util.Scanner;
 
+public class DesenhoRetangulo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Largura do retângulo: ");
+        int largura = scanner.nextInt();
+    }
 }
