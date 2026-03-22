5d6a6b030fe9f5b4cd99a2aee3adede2980a13da
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-22 10:25:10 -0300

    Atualizando

---
+++ Laços de repetição (for, while, do-while)/NumPerf.java
@@ -0,0 +1,6 @@
+public class NumPerf {
+    public static void main(String[] args) {
+        System.out.println("Número perfeitos entre 1 e 1000:");
+        for (int num = 1; num <= 1000; num++)
+    }
+}
