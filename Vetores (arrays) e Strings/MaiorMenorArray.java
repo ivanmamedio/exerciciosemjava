7c035617ee25c231fe7c17678d4213769092e6ab
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-22 15:25:41 -0300

    Atualizando

---
+++ Vetores (arrays) e Strings/MaiorMenorArray.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class MaiorMenorArray {
+    public static void main(String[] args) {
+        Scanner scanner = new scanner(System.in);
+        int[] numeros = new int[5];
+
+        S
+    }
+}
