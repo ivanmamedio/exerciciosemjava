b27dac2b52d57de445dab0196360601caf2f95f0
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-22 15:30:40 -0300

    Corrigi os erros e esqueci do commitar

--- Vetores (arrays) e Strings/MaiorMenorArray.java
+++ Vetores (arrays) e Strings/MaiorMenorArray.java
@@ -2,9 +2,26 @@ import java.util.Scanner;
 
 public class MaiorMenorArray {
     public static void main(String[] args) {
-        Scanner scanner = new scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int[] numeros = new int[5];
 
-        S
+        System.out.println("Digite 5 números:");
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = scanner.nextInt();
+        }
+
+        int maior = numeros[0];
+        int menor = numeros[0];
+
+        for (int num : numeros) {
+            if (num > maior)
+                maior = num;
+            if (num < menor)
+                menor = num;
+        }
+
+        System.out.println("Maior: " + maior);
+        System.out.println("Menor: " + menor);
+        scanner.close();
     }
 }
