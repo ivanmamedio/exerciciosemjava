795cfffe83e85179c3d74fa29f4f61b5d71534e7
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-22 22:48:25 -0300

    Atualizando. Uma parte totalmente nova para mim

--- Vetores e Arrays/OrdenacaoSimples.java
+++ Vetores e Arrays/OrdenacaoSimples.java
@@ -4,5 +4,21 @@ public class OrdenacaoSimples { // Bubble Sort
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] numeros = new int[10];
+
+        System.out.println("Digite 10 números:");
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = scanner.nextInt();
+        }
+
+        // Bubble Sort
+        for (int i = 0; i < numeros.length - 1; i++) {
+            for (int j = 0; j < numeros.length - 1 - i; j++) {
+                if (numeros[j] > numeros[j + 1]) {
+                    int temp = numeros[j];
+                    numeros[j] = numeros[j + 1];
+                    numeros[j + 1] = temp;
+                }
+            }
+        }
     }
 }
