3c66bb2103465f19efdf79d48ba9b1a9af20f021
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-22 22:11:37 -0300

    Atualizando

---
+++ Vetores e Arrays/Palindromo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner; // Verifica se palavra/frase sem espaço é palindromo
+
+public class Palindromo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite uma palavra (sem espaços): ");
+        String texto = scanner.nextLine();
+
+        boolean palindromo = true;
+        int tamanho = texto.length();
+    }
+}
