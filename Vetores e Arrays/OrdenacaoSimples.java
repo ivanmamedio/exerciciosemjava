40e6ac0b305972e2c75e09e34409af5abdadcb87
Carlos Ivan <carlos.ivan1997@outlook.com>
2026-03-22 22:44:42 -0300

    Atualuzando

---
+++ Vetores e Arrays/OrdenacaoSimples.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class OrdenacaoSimples { // Bubble Sort
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[10];
+    }
+}
